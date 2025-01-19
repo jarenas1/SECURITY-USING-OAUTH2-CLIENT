170ca6a6b14ad8b74902a281fbc3b590e5fc7f4b
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-01-19 10:50:58 -0500

    initial commit

---
+++ src/test/java/com/oath/SpringOauth2Client/SpringOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oath.SpringOauth2Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
